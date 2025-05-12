1d8c800d423f5c6c34d4c601896586713cc1a0e7
Michael <200~pregnant-mutt-cape@duck.com>
2025-05-12 16:04:02 -0500

    small website, worked with BenJ

---
+++ static-todo-react-app/src/components/ToDoList.jsx
@@ -0,0 +1,15 @@
+const ToDoList = () => {
+    return(
+       <main>
+        <div>
+            <ul>
+                <li>Learn React</li>
+                <li>Build a project</li>
+                <li>Read documentation</li>
+            </ul>
+        </div>
+       </main> 
+    )
+}
+
+export default ToDoList;
